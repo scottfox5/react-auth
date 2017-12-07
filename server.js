6379f8cee59243f6ff6ce94f3e5702a11d289a40
Scott Fox <scottfox5@gmail.com>
2017-12-07 01:28:58 -0600

    intial commit, basic files/folders added

---
+++ server.js
@@ -0,0 +1,18 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// serve the index page at /
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+var port = process.env.PORT || 5555;
+var server = app.listen(port, function () {
+  console.log('Listening on port ', server.address().port);
+});
