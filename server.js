fe4a5cbe07ffc69cebc41d5536ac7f342ebf3bd9
Scott Fox <scottfox5@gmail.com>
2017-12-07 23:51:43 -0600

    setup environment: babel, webpack

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var express =  require('express');
 var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('app'));
 
 // serve the index page at /
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './app/index.html'));
 });
 
 var port = process.env.PORT || 5555;
